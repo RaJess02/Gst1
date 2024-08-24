319f353564be7cb9a0f7d8fbecb07a731c53ede3
JessicaRavel <JessicaRavel@LAPTOP-JRAVEL>
2024-08-24 15:54:17 -0400

    created demo class

---
+++ src/demo/DemoTest.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class DemoTest {
+	
+	public static void main (String [] args) {
+		System.out.println ("Test  101..");
+		System.out.println (" Test 102");
+		
+	}
+	
+	
+	
+	
+	
+}
